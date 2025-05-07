f7288f787de690944275768707d39d0fd0f34fef
Luis Garcia <102343582+CrimpyWings1800@users.noreply.github.com>
2025-05-07 16:01:34 -0700

    first commit

--- init-zeros.s
+++ init-zeros.s
@@ -1,1 +1,16 @@
+main:
+LDA X0, a // loading the address of an array
+ADDI X1, XZR, #4 // loading the count
+BL InitZeros // calling init zeros
+STOP
 
+InitZeros: // X0 address of the array, X1 is the count value d
+ADDI X9, XZR, #0 // initiating i
+
+loop:
+STUR XZR, [X0, #0] // storing 0 into the array
+ADDI X0, X0, #8 // incrementing array
+ADDI X9, X9, #1 // incrementing count
+CMP X1, X9 // comparing the count and d
+B.LT loop // branching is less than or equal to 
+BR LR // exiting loop
