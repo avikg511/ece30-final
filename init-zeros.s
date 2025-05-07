2f8ad5cc4404570b469353278356cf90811e77fb
Luis Garcia <102343582+CrimpyWings1800@users.noreply.github.com>
2025-05-07 16:31:46 -0700

    Update init-zeros.s

--- init-zeros.s
+++ init-zeros.s
@@ -1,8 +1,9 @@
 main:
-LDA X0, a // loading the address of an array
+//LDA X0, a // loading the address of an array
 ADDI X1, XZR, #4 // loading the count
 BL InitZeros // calling init zeros
-STOP
+B done
+//STOP
 
 InitZeros: // X0 address of the array, X1 is the count value d
 ADDI X9, XZR, #0 // initiating i
@@ -11,6 +12,9 @@ loop:
 STUR XZR, [X0, #0] // storing 0 into the array
 ADDI X0, X0, #8 // incrementing array
 ADDI X9, X9, #1 // incrementing count
-CMP X1, X9 // comparing the count and d
+CMP X9, X1 // comparing the count and d
 B.LT loop // branching is less than or equal to 
 BR LR // exiting loop
+
+done:
+ADDI X3, XZR, #1
